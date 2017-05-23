15c7a652cd31118fb07775c15a5b7e98d6b2a7ce
Daniel Perez Alvarez <danielpa9708@gmail.com>
2017-05-23 19:58:42 -0400

    Add window

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,13 @@
 #include <SDL2/SDL.h>
 
 const SDL_Window * window;
-
 int main() {
+  SDL_Init(SDL_INIT_VIDEO);
+  window = SDL_CreateWindow("Hello World",
+                            SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+                            640, 480,
+                            SDL_WINDOW_SHOWN);
   SDL_Delay(1000);
-  puts("hello world");
+  SDL_Quit();
   return 0;
 }
