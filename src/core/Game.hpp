ec3855475acd76a039c5141f4ff34ba7cb61eacd
David Perez Alvarez <davidpa9708@gmail.com>
2017-05-23 21:32:09 -0400

    Add declaration file for Game

--- src/core/Game.hpp
+++ src/core/Game.hpp
@@ -1,6 +1,16 @@
 #pragma once
+#include "Chopter.hpp"
+#include "Map.hpp"
+
 
 class Game {
+  unsigned long long int steps;
+  int chopterStep;
+  int mapStep;
+  bool canChopterMove () const;
+  bool canMapMove () const;
+  Chopter chopter;
+  Map map;
 public:
   Game();
   void onStep(bool buttonStatus);
