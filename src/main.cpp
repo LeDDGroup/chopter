7ed7fdb0d43a5b306d34f0158b5f18030335152c
Daniel Perez Alvarez <danielpa9708@gmail.com>
2017-05-23 23:21:12 -0400

    Add init and quit helper methods

--- src/main.cpp
+++ src/main.cpp
@@ -4,16 +4,23 @@
 
 void loop();
 
-SDL_Window *window;
+SDL_Window * window;
 Game game;
 
-int main() {
+void init() {
   SDL_Init(SDL_INIT_VIDEO);
   window = SDL_CreateWindow("Chopter",
                             SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
                             640, 480,
                             SDL_WINDOW_SHOWN);
-  loop();
+}
+void quit() {
   SDL_Quit();
+}
+
+int main() {
+  init();
+  loop();
+  quit();
   return 0;
 }
