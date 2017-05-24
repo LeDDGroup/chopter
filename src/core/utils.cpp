9915cf442f3fcb601b2f01b679862f4e46218ee6
Daniel Perez Alvarez <danielpa9708@gmail.com>
2017-05-24 12:01:54 -0400

    Add utils.cpp

---
+++ src/core/utils.cpp
@@ -0,0 +1,11 @@
+#include "utils.hpp"
+
+bool collission(const Rect &a, const Rect &b) {
+  // TODO implement
+  return false;
+}
+
+bool collission(const int x1, const int x2, const int y1, const int y2) {
+  // TODO implement
+  return false;
+}
