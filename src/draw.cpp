42a05b9441da1f9d34d18959f16c2fe22d549a20
Daniel Perez Alvarez <danielpa9708@gmail.com>
2017-05-24 00:08:29 -0400

    Add more colors to draw.cpp

--- src/draw.cpp
+++ src/draw.cpp
@@ -4,6 +4,12 @@
 
 #define C_BLACK 0x00000000
 #define C_WHITE 0xFFFFFFFF
+#define C_RED   0xFFFF0000
+#define C_GREEN 0xFF00FF00
+#define C_BLUE  0xFF0000FF
+
+#define C_SCREEN C_BLACK
+#define C_PLAYER C_BLUE
 
 extern SDL_Window * window;
 extern SDL_Surface * screenSurface;
@@ -14,13 +20,13 @@ static SDL_Rect rect;
 void clearScreen() {
   rect.x = 0; rect.y = 0;
   rect.w = 640; rect.h = 480;
-  SDL_FillRect(screenSurface, &rect, C_BLACK);
+  SDL_FillRect(screenSurface, &rect, C_SCREEN);
 }
 
 void drawChopter(const Chopter & chopter) {
   rect.x = chopter.getX(); rect.y = chopter.getY();
   rect.w = chopter.getWidth(); rect.h = chopter.getHeight();
-  SDL_FillRect(screenSurface, &rect, C_WHITE);
+  SDL_FillRect(screenSurface, &rect, C_PLAYER);
 }
 
 void draw() {
