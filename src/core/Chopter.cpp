fc3639552f7798db37f43abbe33cc1cdfd477661
David Perez Alvarez <davidpa9708@gmail.com>
2017-05-23 23:01:35 -0400

    Add Chopter Basics

--- src/core/Chopter.cpp
+++ src/core/Chopter.cpp
@@ -1,7 +1,38 @@
 #include "Chopter.hpp"
 
-Chopter::Chopter() {
+Chopter::Chopter(int x, int y, int height, int widht) {
+  this->x = x;
+  this->y = y;
+  this->width = width;
+  this->height = height;
+  horizontalSpeed = 1;
+  verticalSpeed = 1;
+  fallSpeed = 1;
 }
-void Chopter::move(bool) {
 
+void Chopter::setPosition (int x, int y) {
+  this->x = x;
+  this->y = y;
+}
+
+void Chopter::fly () {
+  setPosition (x + horizontalSpeed, y - fallSpeed);
+}
+
+void Chopter::fall () {
+  setPosition (x + horizontalSpeed, y + verticalSpeed);
+}
+
+void Chopter::resetPosition () {
+  setPosition (0, y);
+}
+
+void Chopter::move (bool fuel) {
+  if (fuel) {
+    fly ();
+  }
+  else {
+    fall ();
+  }
+  resetPosition ();
 };
--- src/core/Chopter.hpp
+++ src/core/Chopter.hpp
@@ -1,7 +1,17 @@
 #pragma once
 
 class Chopter {
+  int x, y, width, height;
+  int horizontalSpeed, verticalSpeed, fallSpeed;
+  void setPosition (int x, int y);
+  void fly ();
+  void fall ();
+  void resetPosition ();
 public:
-  Chopter();
-  void move(bool);
+  Chopter (int x = 0, int y = 0, int width = 32, int height = 32);
+  void move (bool fuel);
+  int getX ();
+  int getY ();
+  int getWidth ();
+  int getHeight ();
 };
