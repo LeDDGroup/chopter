da1ae0ace23f17b2e9380402188ee6c8525b1889
Daniel Perez Alvarez <danielpa9708@gmail.com>
2017-05-23 22:57:56 -0400

    Change game window title to Chopter

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@ Game game;
 
 int main() {
   SDL_Init(SDL_INIT_VIDEO);
-  window = SDL_CreateWindow("Hello World",
+  window = SDL_CreateWindow("Chopter",
                             SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
                             640, 480,
                             SDL_WINDOW_SHOWN);
