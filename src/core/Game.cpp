eeddeebd748993af566f5f1dce5147b8033e850b
David Perez Alvarez <davidpa9708@gmail.com>
2017-05-23 21:32:43 -0400

    Add definition file for Game

--- src/core/Game.cpp
+++ src/core/Game.cpp
@@ -1,7 +1,26 @@
 #include "Game.hpp"
 
 Game::Game() {
+  steps = 0;
+  chopterStep = 15;
+  mapStep = 20;
+}
+Game::increaseStep() {
+  this->step++;
+}
+Game::canChopterMove() {
+}
+  return true;
+Game::canMapMove() {
+  return true;
 }
 
-void Game::onStep(bool) {
+void Game::onStep(bool moveChopter) {
+  if (canChopterMove()) {
+    this->chopter.move(moveChopter);
+  }
+  if (canMapMove()) {
+    this->map.move();
+  }
+  increaseStep();
 }
