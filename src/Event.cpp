23f15f282c901559b4fa29f2a562b5fa65153b9d
Daniel Perez Alvarez <danielpa9708@gmail.com>
2017-05-23 21:59:38 -0400

    Add event handler for quit action

--- src/Event.cpp
+++ src/Event.cpp
@@ -1,9 +1,27 @@
+#include <SDL2/SDL_events.h>
 #include "Event.hpp"
 
+Event::Event() {
+  quit = false;
+}
+
 bool Event::waitForStepTime() {
-  return false;
+  while(checkEvents()) {}
+  return !quit;
 }
 
 bool Event::checkForButtonDown() const {
   return false;
+}
+
+bool Event::checkEvents() {
+  SDL_Event event;
+  while (!quit && SDL_PollEvent(&event) != 0) {
+    switch (event.type) {
+    case SDL_QUIT:
+      quit = true;
+      break;
+    }
+  }
+  return !quit;
 }
--- src/Event.hpp
+++ src/Event.hpp
@@ -1,7 +1,10 @@
 #pragma once
 
 class Event {
+  bool quit;
+  bool checkEvents();
 public:
+  Event();
   bool waitForStepTime();
   bool checkForButtonDown() const;
 };
