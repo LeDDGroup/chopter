5575cf576100bd9b6d2a425ea751325ec3173706
Daniel Perez Alvarez <danielpa9708@gmail.com>
2017-05-25 05:39:54 -0400

    Add button properties

--- src/control/MainController.cpp
+++ src/control/MainController.cpp
@@ -1,8 +1,17 @@
 #include "MainController.hpp"
 #include "Event.hpp"
+#include "color.hpp"
+
+#define C_SCREEN C_BLACK
 
 MainController::MainController(Logic * logic)
-  : Controller(logic) {};
+  : Controller(logic) {
+  btnPlay.rect.x = 256;
+  btnPlay.rect.y = 64;
+  btnPlay.rect.w = 128;
+  btnPlay.rect.h = 48;
+  btnPlay.color = C_BLUE;
+};
 
 void MainController::loop() {
   Event event;
