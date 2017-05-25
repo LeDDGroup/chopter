4fa44c570f069416a28c96f60a100366fd85716a
Daniel Perez Alvarez <danielpa9708@gmail.com>
2017-05-25 04:35:12 -0400

    Add Controller base class

---
+++ src/control/Controller.cpp
@@ -0,0 +1,6 @@
+#include "Controller.hpp"
+#include "Logic.hpp"
+
+Controller::Controller(Logic * logic)
+  : logic(logic) {
+}
---
+++ src/control/Controller.hpp
@@ -0,0 +1,12 @@
+#ifndef CONTROLLER_HPP
+#define CONTROLLER_HPP
+
+class Logic;
+
+class Controller {
+  Logic * logic;
+public:
+  Controller(Logic * logic);
+};
+
+#endif
