7ebf3aa18c5bde582ff98024af54036e1d18a77d
Daniel Perez Alvarez <danielpa9708@gmail.com>
2017-05-25 04:40:32 -0400

    Make GameController inherit from Controller

--- src/control/Controller.hpp
+++ src/control/Controller.hpp
@@ -6,7 +6,7 @@ class Logic;
 class Controller {
   Logic * logic;
 public:
-  Controller(Logic * logic);
+  Controller(Logic * logic = 0);
   virtual void init() = 0;
   virtual void draw() = 0;
   virtual void loop() = 0;
--- src/control/GameController.hpp
+++ src/control/GameController.hpp
@@ -2,10 +2,11 @@
 #define GAMECONTROLLER_HPP
 
 #include <SDL2/SDL.h>
+#include "Controller.hpp"
 #include "Event.hpp"
 #include "../core/Game.hpp"
 
-class GameController {
+class GameController: public Controller {
   SDL_Window * window;
   SDL_Surface * screenSurface;
   Game game;
@@ -13,8 +14,8 @@ class GameController {
   void drawChopter(const Chopter & chopter);
   void drawColumn(int x, const Hole &hole);
   void drawMap(const Map & map);
-  void draw();
 public:
+  void draw();
   void init();
   void loop();
   void quit();
