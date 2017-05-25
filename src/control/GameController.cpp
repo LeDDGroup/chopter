495e80c5f4099da9aa7c1198f72e0b36c665f68a
Daniel Perez Alvarez <danielpa9708@gmail.com>
2017-05-25 05:08:03 -0400

    Move loop function to GameController implementation file

--- src/control/GameController.cpp
+++ src/control/GameController.cpp
@@ -1,4 +1,16 @@
 #include "GameController.hpp"
+#include "Event.hpp"
 
 GameController::GameController(Logic * logic)
   : Controller(logic) {};
+
+void GameController::loop() {
+  Event event;
+  while(event.waitForStepTime()) {
+    if (!game.hasFinished()) {
+      const bool action = event.checkForButtonDown();
+      game.onStep(action);
+      draw();
+    }
+  }
+}
