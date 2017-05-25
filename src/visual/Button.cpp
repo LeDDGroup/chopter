83eeb8ddd2ea4d4152d276a8fd6dce7c3f8958fb
Daniel Perez Alvarez <danielpa9708@gmail.com>
2017-05-25 06:42:40 -0400

    Add button check click method

--- src/visual/Button.cpp
+++ src/visual/Button.cpp
@@ -3,3 +3,8 @@
 void Button::draw(SDL_Surface * surface) {
   SDL_FillRect(surface, &rect, color);
 }
+
+bool Button::checkClick(const Point<int> &mousePosition) {
+  // TODO implement
+  return false;
+}
--- src/visual/Button.hpp
+++ src/visual/Button.hpp
@@ -3,12 +3,14 @@
 
 #include <SDL2/SDL_video.h>
 #include <SDL2/SDL_rect.h>
+#include "../core/Point.hpp"
 
 class Button {
 public:
   SDL_Rect rect;
   Uint32 color;
   void draw(SDL_Surface * surface);
+  bool checkClick(const Point<int> &mousePosition);
 };
 
 #endif
