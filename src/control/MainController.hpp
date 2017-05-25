087a9f1ad7a69389056827e44188a33fc0570c31
Daniel Perez Alvarez <danielpa9708@gmail.com>
2017-05-25 05:33:51 -0400

    Add draw function and button variable

--- src/control/MainController.hpp
+++ src/control/MainController.hpp
@@ -2,8 +2,11 @@
 #define MAINCONTROLLER_HPP
 
 #include "Controller.hpp"
+#include "../visual/Button.hpp"
 
 class MainController: public Controller {
+  Button btnPlay;
+  void draw();
 public:
   MainController(Logic * logic);
   void loop();
