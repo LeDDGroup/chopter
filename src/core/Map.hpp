e9dc5369467499e229df125ccc0a9af2b2541649
Daniel Perez Alvarez <danielpa9708@gmail.com>
2017-05-25 02:25:09 -0400

    Add definition for check collision method in map class

--- src/core/Map.hpp
+++ src/core/Map.hpp
@@ -1,6 +1,8 @@
 #ifndef MAP_HPP
 #define MAP_HPP
 
+#include "utils.hpp"
+
 #define maxWidth 10000
 
 struct Hole {
@@ -25,6 +27,7 @@ class Map {
 public:
   Map(int height = 15, int length = 21, int updateDistance = -1);
   void onStep(int position);
+  bool checkCollision(const Rect &rect) const;
   int pxToBlock(int x, int blockWidth);
   int getLength() const { return this->length; }
   const Hole * getField() const { return this->field; }
