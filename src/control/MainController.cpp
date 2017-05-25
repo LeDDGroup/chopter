7df9cc7bd5a30d62469a337c3e3a552ea484cb7f
Daniel Perez Alvarez <danielpa9708@gmail.com>
2017-05-25 05:21:46 -0400

    Add MainController

---
+++ src/control/MainController.cpp
@@ -0,0 +1,4 @@
+#include "MainController.hpp"
+
+MainController::MainController(Logic * logic)
+  : Controller(logic) {};
---
+++ src/control/MainController.hpp
@@ -0,0 +1,11 @@
+#ifndef MAINCONTROLLER_HPP
+#define MAINCONTROLLER_HPP
+
+#include "Controller.hpp"
+
+class MainController: public Controller {
+public:
+  MainController(Logic * logic);
+};
+
+#endif
