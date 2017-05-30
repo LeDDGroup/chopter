bbac7e030135deb0025e5611e015014f684f8c00
Daniel Perez Alvarez <danielpa9708@gmail.com>
2017-05-30 11:18:55 -0400

    Add sprite draw

--- src/visual/Sprite.cpp
+++ src/visual/Sprite.cpp
@@ -1,8 +1,13 @@
 #include "Sprite.hpp"
 #include "../control/Environment.hpp"
+#include <SDL2pp/Renderer.hh>
 
 extern Environment environment;
 
 Sprite::Sprite(const char * path)
   : texture(environment.renderer, path) {
+}
+
+void Sprite::draw(const SDL2pp::Rect &rect) {
+  environment.renderer.Copy(texture, SDL2pp::NullOpt, rect);
 }
--- src/visual/Sprite.hpp
+++ src/visual/Sprite.hpp
@@ -1,7 +1,9 @@
 #include <SDL2pp/Texture.hh>
+#include <SDL2pp/Rect.hh>
 
 class Sprite {
   SDL2pp::Texture texture;
 public:
   Sprite(const char * path);
+  void draw(const SDL2pp::Rect &rect);
 };
